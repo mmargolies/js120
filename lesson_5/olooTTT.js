d5bbd5d7f33c21cdd16c140a71cf639826d5b3db
Michael Margolies <mmargolies978@gmail.com>
2023-07-01 14:05:08 -0400

    Game object added

--- lesson_5/olooTTT.js
+++ lesson_5/olooTTT.js
@@ -86,12 +86,16 @@ const PlayerPrototype = {
   getMarker() {
     return this.marker;
   },
+
+  test() {
+    console.log('fuck');
+  },
 };
 
 const Human = Object.create(PlayerPrototype);
 
 Human.init = function() {
-  this.initalize(Square.HUMAN_MARKER);
+  return this.initialize(Square.HUMAN_MARKER);
 };
 
 const Computer = Object.create(PlayerPrototype);
@@ -99,3 +103,146 @@ const Computer = Object.create(PlayerPrototype);
 Computer.init = function() {
   return this.initialize(Square.COMPUTER_MARKER);
 };
+
+const TTTgame = {
+  WINNING_COMBOS: [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+    [1, 5, 9], [3, 5, 7]             // diagonals
+  ],
+
+  WINNING_NUM: 3,
+
+  init() {
+    this.board = Object.create(Board).init();
+    this.human = Object.create(Human).init();
+    this.computer = Object.create(Computer).init();
+
+    return this;
+  },
+
+  play() {
+    this.displayWelcomeMessage();
+    this.displayRules();
+    this.waitForAcknowledgement();
+
+    do {
+      this.reset();
+
+      while (true) {
+        this.board.display();
+
+        this.humanMoves();
+        if (this.gameOver()) break;
+
+        this.computerMoves();
+        if (this.gameOver()) break;
+      }
+
+      this.board.display();
+      this.displayResults();
+      this.waitForAcknowledgement();
+    } while (this.playAgain());
+
+    this.displayGoodbyeMessage();
+  },
+
+  prompt(text) {
+    console.log(`>> ${text}`);
+  },
+
+  joinOr(arr, delim = ', ', word = 'or') {
+    if (arr.length < 2) return arr.join('');
+
+    return arr.slice(0, (arr.length - 1)).join(delim) +
+           `${delim}${word} ${arr[arr.length - 1]}`;
+  },
+
+  clearScreen() {
+    console.clear();
+  },
+
+  displayWelcomeMessage() {
+    this.clearScreen();
+    this.prompt('Hello & welcome to Tic-Tac-Toe!\n');
+  },
+
+  displayRules() {
+    this.prompt('3 in a row wins!');
+  },
+
+  waitForAcknowledgement() {
+    rlSync.question(
+      '(Press Enter to continue...)', {hideEchoBack: true, mask: ''}
+    );
+    this.clearScreen();
+  },
+
+  humanMoves() {
+    let validChoices = this.board.unusedSquares();
+    this.prompt(`Pick a square (${this.joinOr(validChoices)}):`);
+    let choice = rlSync.question().trim();
+
+    while (!validChoices.includes(choice)) {
+      this.prompt(
+        `Choose a valid square (${this.joinOr(validChoices)}):`
+      );
+      choice = rlSync.question().trim();
+    }
+
+    this.board.markSquareAt(choice, this.human.getMarker());
+  },
+
+  computerMoves() {
+    let validChoices = this.board.unusedSquares();
+    let choiceIdx = Math.floor(Math.random() * validChoices.length);
+
+    this.board.markSquareAt(validChoices[choiceIdx], this.computer.getMarker());
+  },
+
+  someoneWon() {
+    return this.detectWinner(this.human) || this.detectWinner(this.computer);
+  },
+
+  detectWinner(player) {
+    return TTTgame.WINNING_COMBOS.some(combo => {
+      return this.board.countMarkersFor(player, combo) === TTTgame.WINNING_NUM;
+    });
+  },
+
+  gameOver() {
+    return this.board.isFull() || this.someoneWon();
+  },
+
+
+  displayResults() {
+    if      (this.detectWinner(this.human))    this.prompt('You won!! Nice :)');
+    else if (this.detectWinner(this.computer)) this.prompt('Computer wins...');
+    else                                       this.prompt('Oh. A tie.');
+  },
+
+  reset() {
+    this.board = Object.create(Board).init();
+  },
+
+  playAgain() {
+    this.prompt('Would you like to play again? (yes/no, y/n)');
+    let choice = rlSync.prompt().trim().toLowerCase();
+
+    while (!['yes', 'no', 'y', 'n'].includes(choice)) {
+      this.prompt('Please enter a valid choice (yes/no, y/n)');
+      choice = rlSync.prompt().trim().toLowerCase();
+    }
+
+    return choice === 'yes' || choice === 'y';
+  },
+
+  displayGoodbyeMessage() {
+    this.clearScreen();
+    this.prompt('Goodbye, thank you for playing! :)');
+  }
+};
+
+let ticTacToe = Object.create(TTTgame).init();
+
+ticTacToe.play();
