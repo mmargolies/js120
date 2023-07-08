5fe16b958516636e7e4726d0693cd3e5ae2ab3e4
Michael Margolies <mmargolies978@gmail.com>
2023-07-08 12:18:53 -0400

    Implemented 'wallet' for human player, changed win detection/display, changed static properties storing valid responses/response validation. Still need to decide on how/where/under what conditions to generate a new deck.

--- lesson_5/ooTwentyone.js
+++ lesson_5/ooTwentyone.js
@@ -1,8 +1,8 @@
 // LEFT OFF:
-// implementing playGame, aka the 'full' game loop
+// Adjusting static properties for responses
 
 // TODO:
-// implement betting
+// re-generate deck if it is empty
 
 const rlSync  = require('readline-sync');
 const shuffle = require('shuffle-array');
@@ -11,7 +11,9 @@ class Card {
   static SUITS       = ['C', 'S', 'H', 'D' ]
   static FACE_CARDS  = ['J', 'Q', 'K',];
   static ACE         = 'A'
-  static CARD_VALUES = [ 2, 3, 4, 5, 6, 7, 8, 9, ...Card.FACE_CARDS, Card.ACE];
+  static CARD_VALUES = [
+    2, 3, 4, 5, 6, 7, 8, 9, 10, ...Card.FACE_CARDS, Card.ACE
+  ];
 
   constructor(value, suit) {
     this.value = value;
@@ -32,11 +34,6 @@ class Card {
 
   toString() {
     return `[${this.value}|${this.suit}]`;
-    // return `
-    // +---+
-    // |${this.value} ${this.suit}|
-    // +---+
-    // `;
   }
 }
 
@@ -82,6 +79,27 @@ const Hand = {
 class Human {
   constructor() {
     this.resetHand();
+    this.wallet = 5;
+  }
+
+  getBalance() {
+    return this.wallet;
+  }
+
+  addToWallet() {
+    this.wallet += 1;
+  }
+
+  subtractFromWallet() {
+    this.wallet -= 1;
+  }
+
+  isBroke() {
+    return this.wallet <= 0;
+  }
+
+  isRich() {
+    return this.wallet >= 10;
   }
 }
 
@@ -105,10 +123,12 @@ class Dealer {
 Object.assign(Dealer.prototype, Hand);
 
 class TwentyOneGame {
-  static HIT_STAY_RESPONSES = ['h', 'hit', 's', 'stay'];
-  static YES_NO_RESPONSES   = ['y', 'yes', 'n', 'no']
-  static TARGET_SCORE       = 21;
-  static DEALER_LIMIT       = 17;
+  static HIT            = ['h', 'hit'];
+  static STAY           = ['s', 'stay'];
+  static YES            = ['y', 'yes']
+  static NO             = ['n', 'no']
+  static TARGET_SCORE   = 21;
+  static DEALER_LIMIT   = 17;
 
   constructor() {
     this.human = new Human();
@@ -116,25 +136,24 @@ class TwentyOneGame {
     this.deck = new Deck();
   }
 
-  playGame() {
+  play() {
     this.displayWelcomeMessage();
     this.displayRules();
     this.waitForAcknowledgement();
 
     do {
       this.playHand();
+
+      if (this.human.isBroke() || this.human.isRich()) break;
     } while (this.keepPlaying());
 
     this.displayGoodbyeMessage();
   }
 
   playHand() {
-    // this.displayWelcomeMessage();
-    // this.displayRules();
-    // this.waitForAcknowledgement();
-
     this.dealHand();
     this.displayCards();
+    this.displayBalance();
     this.humanTurn();
 
     if (!this.busted(this.human)) {
@@ -142,8 +161,8 @@ class TwentyOneGame {
     }
 
     this.displayResults();
+    this.updateWallet();
     this.waitForAcknowledgement();
-    // this.displayGoodbyeMessage();
   }
 
   prompt(text) {
@@ -184,10 +203,22 @@ class TwentyOneGame {
       'Aces have a value of 1 or 11 (the best value will be chosen for you).'
     );
     this.prompt('Face cards have a value of 10.\n');
+
+    this.prompt('You start the game with $5. Each time you win, you get $1!');
+    this.prompt('Each time you lose, you lose $1. If you get to $10, you win!');
+    this.prompt('If your balance goes to $0, you lose the game.\n');
     this.prompt('Ready?');
   }
 
   dealHand() {
+    // what to do about a potentially empty deck?
+    // new deck every hand? Not a fan of this option
+
+    // ideally, there's a way to detect when the deck is empty, and use that
+    //    condition to generate a new deck. Can't do that in dealCard though...
+    //    The trick here is being in the correct scope to instantiate a new deck
+    //    but not have to call a method within the game class each time a card
+    //    is dealt.
     this.human.resetHand();
     this.dealer.resetHand();
 
@@ -206,17 +237,20 @@ class TwentyOneGame {
     console.log(`Total: ${this.calcHandTotal(this.human)}\n`);
   }
 
+  displayBalance() {
+    console.log(`Your wallet has: $${this.human.getBalance()}\n`);
+  }
+
   keepHitting() {
     this.prompt('Would you like to hit or stay? (h/hit, s/stay)');
     let choice = this.getLowercaseUserChoice();
 
-    while (!TwentyOneGame.HIT_STAY_RESPONSES.includes(choice)) {
+    while (![...TwentyOneGame.HIT, ...TwentyOneGame.STAY].includes(choice)) {
       this.prompt('Please enter a valid choice (h/hit, s/stay)');
       choice = this.getLowercaseUserChoice();
     }
 
-    return choice === 'h' || choice === 'hit';
-    // kinda defeats the purpose of the static property, no?
+    return TwentyOneGame.HIT.includes(choice);
   }
 
   determineValue(card) {
@@ -246,6 +280,7 @@ class TwentyOneGame {
     while (this.keepHitting()) {
       this.human.addToHand(this.deck.dealCard());
       this.displayCards();
+      this.displayBalance();
 
       if (this.busted(this.human)) break;
     }
@@ -262,20 +297,35 @@ class TwentyOneGame {
     }
   }
 
-  displayWinner() {
+  determineWinner() {
     if (this.busted(this.human)) {
-      this.prompt('Busted...the dealer wins this hand!');
+      return 'humanBust';
     } else if (this.busted(this.dealer)) {
-      this.prompt('The dealer busted -- you won the hand!');
+      return 'dealerBust';
     } else {
       let humanScore = this.calcHandTotal(this.human);
       let dealerScore = this.calcHandTotal(this.dealer);
 
-      if (humanScore > dealerScore) this.prompt('You won the hand!');
-      else if (humanScore < dealerScore) this.prompt('You lost the hand.');
-      else if (humanScore === dealerScore) this.prompt("This hand's a tie.");
-    }
+      if (humanScore > dealerScore) return 'human';
+      else if (humanScore < dealerScore) return 'dealer';
 
+      return 'tie';
+    }
+  }
+
+  displayWinner() {
+    switch (this.determineWinner()) {
+      case 'humanBust': this.prompt('Busted...the dealer wins this hand!');
+        break;
+      case 'dealerBust': this.prompt('The dealer busted -- you won the hand!');
+        break;
+      case 'human': this.prompt('You won the hand!');
+        break;
+      case 'dealer': this.prompt('You lost the hand.');
+        break;
+      case 'tie': this.prompt("This hand's a tie.");
+        break;
+    }
   }
 
   displayResults() {
@@ -289,25 +339,47 @@ class TwentyOneGame {
     this.displayWinner();
   }
 
+  updateWallet() {
+    switch (this.determineWinner()) {
+      case 'human':
+      case 'dealerBust':
+        this.human.addToWallet();
+        break;
+      case 'dealer':
+      case 'humanBust':
+        this.human.subtractFromWallet();
+        break;
+    }
+  }
+
   keepPlaying() {
     this.prompt('Would you like to keep playing? (y/yes, n/no)');
     let choice = this.getLowercaseUserChoice();
 
-    while (!TwentyOneGame.YES_NO_RESPONSES.includes(choice)) {
+    while (![...TwentyOneGame.YES, ...TwentyOneGame.NO].includes(choice)) {
       this.prompt('Please enter a valid choice (y/yes, n/no))');
       choice = this.getLowercaseUserChoice();
     }
 
-    return choice === 'y' || choice === 'yes';
-    // kinda defeats the purpose of the static property, no?
+    return TwentyOneGame.YES.includes(choice);
   }
 
   displayGoodbyeMessage() {
     this.clearScreen();
-    this.prompt('Goodbye, thanks for playing! :)');
+
+    if (this.human.isBroke()) {
+      this.prompt("You're broke! Get outta here!!");
+    } else if (this.human.isRich()) {
+      this.prompt("You're rich! Enjoy the high life B-)");
+    } else {
+      this.prompt('Goodbye, thanks for playing! :)');
+      console.log(
+        `You walked away from the table with $${this.human.getBalance()}.`
+      );
+    }
   }
 }
 
 let twentyOne = new TwentyOneGame();
 
-twentyOne.playGame();
+twentyOne.play();
